aacea3c8f8e18d061a1209db04138ce3f0b0dafb
Leonardo Borges <76793724+leonardob0rges@users.noreply.github.com>
2022-07-30 00:02:05 -0300

    Add files via upload

---
+++ src/1-7/Api1.java
@@ -0,0 +1,36 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
+public class Api1 {
+    public static void main(String[] args) throws Exception{
+        /* Objetivo: consumir a API do IMDB (api alternativa) e imprimir os
+        resultados de uma busca dos filmes. */
+
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+
+        HttpClient client = HttpClient.newHttpClient(); // instanciando o cliente http
+        HttpRequest.Builder builder = HttpRequest.newBuilder() // criando requisição e definindo parâmetros
+                .GET().timeout(Duration.ofSeconds(10)) // criando timeout de resposta de 10 seg
+                .uri(URI.create(url)); // criando URI e atribuindo solicitação à url destino
+        var request = builder.build();
+        HttpResponse<String> response = client  // construindo a requisição com resposta no tipo string
+                .send(request,HttpResponse.BodyHandlers.ofString());
+
+        var statusCode = response.statusCode();
+        var headers = response.headers();
+        var body = response.body();
+
+        if(statusCode == 200){
+            System.out.println("Requisição bem sucedida. Status code: " + statusCode);
+        } else{
+            System.out.println("Falha na requisição. Tente novamente");
+        }
+
+        //System.out.println("- O status code é:\n " + statusCode);
+        System.out.println("\n- Headers da api:\n " + headers);
+        System.out.println("- Resultado api:\n" + body);
+    }
+}
